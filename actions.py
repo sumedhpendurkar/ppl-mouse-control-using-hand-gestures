9fa49e494057e27d9512f63ec49924c87fbcda42
Sumedh Pendurkar <sumedh.pendurkar@gmail.com>
2017-04-06 18:12:29 +0530

    a smooth transition in mouse moment; resumes from the current mouse position

--- actions.py
+++ actions.py
@@ -3,22 +3,26 @@ class action:
     left_click = 1
     right_click = 2
     middle = 3
+    prev_coordinates = [-1, -1]
     prev_state = ''
-    screen_x = 0
-    screen_y = 0
     def __init__(self, img_shape):
         self.mouse_cntrl = PyMouse()
-        shape_x, shape_y, _ = img_shape
+        shape_y, shape_x, _ = img_shape
         print shape_x, shape_y, self.mouse_cntrl.screen_size()
-        self.screen_x, self.screen_y = self.mouse_cntrl.screen_size()
-        self.x_convert = self.screen_x * 1.0 / shape_y 
-        self.y_convert = self.screen_y * 1.0 / shape_x
-        print shape_x, shape_y, self.screen_x, self.screen_y
+        screen_x, screen_y = self.mouse_cntrl.screen_size()
+        self.x_convert = screen_x * 1.0 / shape_x 
+        self.y_convert = screen_y * 1.0 / shape_y
     def screen_size(self):
         return self.mouse_cntrl.screen_size()
     def one(self, hull_p):
-        self.mouse_cntrl.move(self.x_convert * hull_p[0],self.y_convert * hull_p[1])
-        self.prev_state = 'one'
+        if self.prev_state == 'one':
+            x,y = self.mouse_cntrl.position()
+            self.mouse_cntrl.move(x - self.prev_coordinates[0] + hull_p[0], 
+                y - self.prev_coordinates[1] + hull_p[1])
+        else:
+            self.prev_coordinates[0] = hull_p[0]
+            self.prev_coordinates[1] = hull_p[1]
+            self.prev_state = 'one'
     def two(self):
         print "don"
         if self.prev_state == 'two':
